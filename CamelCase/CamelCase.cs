08c812694f74e9d859257d3b108518c5ba6fd727
Kaan Furkan ÇAKIROĞLU <kcakiroglu@innova.com.tr>
2023-05-17 11:26:35 +0300

    CamelCase challange solved !

---
+++ CamelCase/CamelCase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CamelCase
+{
+    public class Result
+    {
+        public static int camelcase(string s)
+        {
+            int result = 1;
+
+            foreach (var letter in s)
+            {
+                if (letter >= 65 && letter <= 90)
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}
