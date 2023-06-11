a7988e8be3a1525cf99edc08f77828b31c639798
Kaan Furkan Çakıroğlu <kfc0737@gmail.com>
2023-06-11 16:08:42 +0300

    Intro To Tutorial Challenges solved !

---
+++ Intro to Tutorial Challenges/IntroToTutorialChallenges.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroToTutorialChallenges
+{
+    public class Result
+    {
+        public static int introTutorial(int V, List<int> arr)
+        {
+            return arr.IndexOf(V);
+        }
+    }
+}
